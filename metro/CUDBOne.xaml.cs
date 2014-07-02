2cd0f68adec0ad7bce3f5464d8512740569ee303
cjin <chong.jin@zafinlabs.com>
2014-07-02 02:04:22 -0400

    done cudb file

---
+++ metro/CUDBOne.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace metro
+{
+    /// <summary>
+    /// Interaction logic for CUDBOne.xaml
+    /// </summary>
+    public partial class CUDBOne : UserControl
+    {
+        public CUDBOne()
+        {
+            InitializeComponent();
+        }
+
+        private void analyzeButton_click(object sender, RoutedEventArgs e)
+        {
+            HlrFileProcess hlrFileProcess = new HlrFileProcess();
+            LoadDataSet loadDataSet = hlrFileProcess.processHLRLogFile("F:\\ProjectMTest\\log\\2014-06-26-12-CUDB01.txt");
+        }
+    }
+}
---
+++ metro/CudbFileProcess.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace metro
+{
+    public class CudbDataSet
+    {
+        public String hlrUserCount;
+        public String hlrUserActCount;
+        public String gprsUserCount;
+        public String authInfoCount;
+        public String twoGAuthInfoCount;
+        public String threeGAuthInfoCount;
+        public String fourGUserCount;
+    }
+
+
+    class CudbFileProcess
+    {
+        public CudbDataSet processCudbLogFile(String filePath)
+        {
+            CudbDataSet cudbDataSet = new CudbDataSet();
+
+            System.IO.StreamReader file =
+                    new System.IO.StreamReader(filePath);
+
+            List<string> fileLines = new List<string>();
+
+            int hlrUserCountLine = 0;
+            int hlrUserActCountLine = 0;
+            int gprsUserCountLine = 0;
+            int authInfoCountLine = 0;
+            int twoGAuthInfoCountLine = 0;
+            int threeGAuthInfoCountLine = 0;
+            int fourGUserCountLine = 0;
+
+            string line;
+
+            int count = 0;
+            while ((line = file.ReadLine()) != null)
+            {
+                if(line.Contains
+            }
+
+            return cudbDataSet;
+        }
+    }
+}
