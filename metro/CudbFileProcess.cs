022b854e73260f2f5c98909e4069dc829cd7e906
cjin <chong.jin@zafinlabs.com>
2014-07-03 23:55:09 -0400

    added CUDB analyzing functions

--- metro/CudbFileProcess.cs
+++ metro/CudbFileProcess.cs
@@ -28,23 +28,72 @@ namespace metro
 
             List<string> fileLines = new List<string>();
 
-            int hlrUserCountLine = 0;
-            int hlrUserActCountLine = 0;
-            int gprsUserCountLine = 0;
-            int authInfoCountLine = 0;
-            int twoGAuthInfoCountLine = 0;
-            int threeGAuthInfoCountLine = 0;
-            int fourGUserCountLine = 0;
+            //int hlrUserCountLine = 0;
+            //int hlrUserActCountLine = 0;
+            //int gprsUserCountLine = 0;
+            //int authInfoCountLine = 0;
+            //int twoGAuthInfoCountLine = 0;
+            //int threeGAuthInfoCountLine = 0;
+            //int fourGUserCountLine = 0;
 
             string line;
 
             int count = 0;
             while ((line = file.ReadLine()) != null)
             {
-                if(line.Contains
+                if (line.Contains("HLRSUBS; NMOBSUBSCNT"))
+                {
+                    cudbDataSet.hlrUserCount = filterData(line);
+                }
+
+                if (line.Contains("HLRSUBS; NACTSUBSCNT") && count == 0)
+                {
+                    cudbDataSet.hlrUserActCount = filterData(line);
+                    count++;
+                }
+
+                if (line.Contains("NAMSUBS; BOTHGPRSCNT") )
+                {
+                    cudbDataSet.gprsUserCount = filterData(line);
+                }
+
+                if (line.Contains("AUCSUBS; NSUBSCNT"))
+                {
+                    cudbDataSet.authInfoCount = filterData(line);
+                }
+
+                if (line.Contains("AUCSUBS; NGSUBSCNT"))
+                {
+                    cudbDataSet.twoGAuthInfoCount = filterData(line);
+                }
+
+                if (line.Contains("AUCSUBS; NUSUBSCNT") )
+                {
+                    cudbDataSet.threeGAuthInfoCount = filterData(line);
+                }
+
+                if (line.Contains("HssEsmUsersStored; TotalNumberOfEpsUsersStored"))
+                {
+                    cudbDataSet.fourGUserCount = filterData(line);
+                }
+
             }
 
             return cudbDataSet;
+        }
+
+        private string filterData(string line)
+        {
+            string[] chars = line.Split(';');
+
+            if (chars.Count() == 4)
+            {
+                return chars[chars.Count() - 1];
+            }
+            else
+            {
+                return "N/A";
+            }
         }
     }
 }
