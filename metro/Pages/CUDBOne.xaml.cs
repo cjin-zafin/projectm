d72132cb2f165b21c9ad94faecbdf6b90612fb86
cjin <chong.jin@zafinlabs.com>
2014-07-05 02:16:00 -0400

    added HSS analyzer and proved that xml reader works

---
+++ metro/Pages/CUDBOne.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using metro.Processors;
+
+namespace metro
+{
+    /// <summary>
+    /// Interaction logic for CUDBOne.xaml
+    /// </summary>
+    public partial class CUDBOne : UserControl
+    {
+        public CUDBOne()
+        {
+            InitializeComponent();
+        }
+
+        protected class FirstGridData
+        {
+            public string HlrUserCount { get; set; }
+            public string HlrActiveUserCount { get; set; }
+            public string GpsUserCount { get; set; }
+            public string AuthInfoCount { get; set; }
+        }
+
+        protected class SecontGridData
+        {
+            public String TwoGAuthInfoCount { get; set; }
+            public String ThreeGAuthInfoCount { get; set; }
+            public String fourGUserCount { get; set; }
+        }
+
+        private void analyzeButton_click(object sender, RoutedEventArgs e)
+        {
+            CudbFileProcess cudbFileProcess = new CudbFileProcess();
+            CudbDataSet loadDataSet = cudbFileProcess.processCudbLogFile("F:\\ProjectMTest\\log\\2014-06-26-12-CUDB01.txt");
+
+            List<FirstGridData> firstData = new List<FirstGridData>();
+            FirstGridData fd = new FirstGridData();
+            fd.HlrUserCount = loadDataSet.hlrUserCount;
+            fd.HlrActiveUserCount = loadDataSet.hlrUserActCount;
+            fd.GpsUserCount = loadDataSet.gprsUserCount;
+            fd.AuthInfoCount = loadDataSet.authInfoCount;
+
+            firstData.Add(fd);
+            firstGrid.ItemsSource = firstData;
+
+            List<SecontGridData> secondData = new List<SecontGridData>();
+            SecontGridData sd = new SecontGridData();
+            sd.fourGUserCount = loadDataSet.fourGUserCount;
+            sd.ThreeGAuthInfoCount = loadDataSet.threeGAuthInfoCount;
+            sd.TwoGAuthInfoCount = loadDataSet.twoGAuthInfoCount;
+
+            secondData.Add(sd);
+            secondGrid.ItemsSource = secondData;
+        }
+    }
+}
---
+++ metro/Processors/HSSProcessor.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace metro.Processors
+{
+    public class HssDataSet
+    {
+        public String maxCpuLoad;
+        public String MaxMemUsage;
+        public String updateLocation;
+        public String cancelLocation;
+        public String sctpResendRate;
+        public String sentAuthenticationInfo;
+        public String exDbModify;
+        public String exDbSearch;
+    }
+
+    class HSSProcessor
+    {
+        public HssDataSet processHssFileSet(List<String> filePathes)
+        {
+            HssDataSet hsData = new HssDataSet();
+
+            foreach (String filePath in filePathes)
+            {
+                if (filePath.Contains("PlatformMeasures"))
+                {
+                    processPlatformMeasureFile(hsData, filePath);
+                }
+                else if (filePath.Contains("HSS-ESM"))
+                {
+                    processEsmFile(hsData, filePath);
+                }
+                else if (filePath.Contains("SS7Statistics"))
+                {
+                    processSs7File(hsData, filePath);
+                }
+            }
+            return hsData;
+        }
+
+        private void processSs7File(HssDataSet hsData, string filePath)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void processEsmFile(HssDataSet hsData, string filePath)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void processPlatformMeasureFile(HssDataSet hsData, String filePath)
+        {
+            System.IO.StreamReader file =
+                   new System.IO.StreamReader(filePath);
+
+            List<String> lines = new List<string>();
+            int systemLineNumber = 0;
+            int count = 0;
+
+            string line;
+            while ((line = file.ReadLine()) != null)
+            {
+                lines.Add(line);
+                if (line.Contains("PlatformMeasures=DEFAULT, Source = _SYSTEM"))
+                {
+                    systemLineNumber = count;
+                }
+
+                count++;
+            }
+
+            String sysLine = lines[systemLineNumber];
+
+            XmlReader xReader = XmlReader.Create(new StringReader(sysLine));
+
+            Debug.WriteLine(xReader.ReadString() + "33 \n");
+
+            while (xReader.Read())
+            {
+                 if (xReader.NodeType == XmlNodeType.Element
+                    && xReader.Name == "moid")
+                 {
+                     Debug.WriteLine(xReader.ReadString() + "11 \n");
+                 }
+            }
+        }
+    }
+}
